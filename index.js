4fdabb7108a62af7826e5bb981e20cc464a5eed5
Pallavi <pallavi3749@gmail.com>
2023-11-20 13:34:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.listen(3002, ()=>{
+    console.log(`Successfully started the server on port`);
+    //Logger.info("Successfully started the server",{});
+
+});
