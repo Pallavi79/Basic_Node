772d9fba26de6e47369e7c46d3d150c45258e4bf
Pallavi <pallavi3749@gmail.com>
2023-11-20 13:42:27 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/about', (req, res) => {
+    res.send('About Us Page');
+  });
+
 app.listen(3002, ()=>{
-    console.log(`Successfully started the server on port`);
+    console.log(`Successfully started the server`);
     //Logger.info("Successfully started the server",{});
 
 });
